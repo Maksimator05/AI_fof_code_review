64f5fd7c1444575df4657e62258a9ca71f41c852
Timur-Git <uchebapraktikum-t1@yandex.ru>
2026-01-17 03:42:43 +0300

    Added IP host as .env config; Fix main.py & README.md

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
 
 from backend.app.db.create_tables import create_tables
 from backend.app.routers.endpoints import router
+
+load_dotenv()
+HOST_IP = os.getenv("IP")
+if not HOST_IP:
+    raise ValueError("IP not set in .env")
 
 app = FastAPI(
     title="AI code review API",
@@ -28,7 +35,7 @@ create_tables()
 if __name__ == "__main__":
     uvicorn.run(
         app=app,
-        host="127.0.0.1",
+        host=HOST_IP,
         port=8080,
         log_level="info"
     )
